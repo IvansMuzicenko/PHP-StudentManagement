58b0b3d908557284bb8d8f056d0c97496fe4e72c
IvansMuzicenko <ivans.muzicenko@gmail.com>
2022-06-27 16:58:09 +0300

    feat: database connection and run

--- private/core/database.php
+++ private/core/database.php
@@ -1,1 +1,35 @@
 <?php
+
+class Database {
+    private function connect() {
+        $db_params = "mysql:host=localhost;dbname=university_db";
+        if ($con = new PDO($db_params, 'root', '')) {
+            die("Failed to connect to database");
+        }
+
+        return $con;
+    }
+
+    public function run($query, $data = [], $data_type = 'object') {
+        $con = $this->connect();
+        $stm = $con->prepare($query);
+
+        if ($stm) {
+            $check = $stm->execute();
+            if ($check) {
+                if ($data_type == 'object') {
+                    $data = $stm->fetchAll(PDO::FETCH_OBJ);
+                } else {
+                    $data = $stm->fetchAll(PDO::FETCH_ASSOC);
+                }
+
+                if (is_array($data) && count($data) > 0) {
+                    return $data;
+                }
+            }
+        }
+    }
+
+    private function query() {
+    }
+}
