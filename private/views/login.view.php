a8f2b55f527fca6988e3cc1dad56212eb17f5292
IvansMuzicenko <ivans.muzicenko@gmail.com>
2022-06-27 11:48:59 +0300

    feat: signup conroller and view

--- private/views/login.view.php
+++ private/views/login.view.php
@@ -3,8 +3,8 @@
         <h2 class="text-center">My University</h2>
         <img src="<?= ROOT; ?>/assets/logo.png" class="rounded-circle w-50 d-block mx-auto border border-dark">
         <h3>Login</h3>
-        <input type="email" class="form-control" name="email" placeholder="Email" autofocus>
-        <input type="password" class="form-control" name="password" placeholder="Password">
+        <input type="email" class="my-2 form-control" name="email" placeholder="Email" autofocus>
+        <input type="password" class="my-2 form-control" name="password" placeholder="Password">
         <br>
         <button class="btn btn-primary">Login</button>
     </div>
