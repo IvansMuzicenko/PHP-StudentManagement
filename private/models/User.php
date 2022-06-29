2ec494b2b41fad1973a762f959d601a213c7a41a
IvansMuzicenko <ivans.muzicenko@gmail.com>
2022-06-29 09:33:10 +0300

    feat: signup validations

--- private/models/User.php
+++ private/models/User.php
@@ -1,5 +1,45 @@
 <?php
 
 class User extends Model {
-    // protected $table = "Users";
+    public function validate($DATA) {
+        $this->errors = [];
+
+
+        if (empty($DATA['firstname'])) {
+            $this->errors['firstname'] = "Please provide a firstname";
+        } else if (!preg_match("/^[a-zA-Z]+$/", $DATA['firstname'])) {
+            $this->errors['firstname'] = "Only letters allowed in first name";
+        }
+
+        if (empty($DATA['lastname'])) {
+            $this->errors['lastname'] = "Please provide a lastname";
+        } else if (!preg_match("/^[a-zA-Z]+$/", $DATA['lastname'])) {
+            $this->errors['lastname'] = "Only letters allowed in lastname";
+        }
+
+        if (empty($DATA['email'])) {
+            $this->errors['email'] = "Please provide a email";
+        } else if (!filter_var($DATA['email'], FILTER_VALIDATE_EMAIL)) {
+            $this->errors['email'] = "Email is not valid";
+        }
+
+        if (empty($DATA['gender'])) {
+            $this->errors['gender'] = "Please provide a gender";
+        }
+
+        if (empty($DATA['rank'])) {
+            $this->errors['rank'] = "Please provide a rank";
+        }
+
+        if (empty($DATA['password']) || empty($DATA['password2'])) {
+            $this->errors['password'] = "Passwords must not be empty";
+        } else if ($DATA['password'] != $DATA['password2']) {
+            $this->errors['password'] = "Passwords do not match";
+        }
+
+        if (count($this->errors) == 0) {
+            return true;
+        }
+        return false;
+    }
 }
--- private/views/signup.view.php
+++ private/views/signup.view.php
@@ -1,32 +1,32 @@
-    <div class="container-fluid">
-        <form action="" method="POST">
-            <div class="p-3 mt-2 mx-auto shadow rounded" style="width: 100%; max-width: 310px;">
-                <h2 class="text-center">My University</h2>
-                <img src="<?= ROOT; ?>/assets/logo.png" class="rounded-circle w-50 d-block mx-auto border border-dark">
-                <h3>Add user</h3>
-                <input type="firstname" class="my-2 form-control" name="firstname" placeholder="Firstname" autofocus>
-                <input type="lastname" class="my-2 form-control" name="lastname" placeholder="Lastname">
+<div class="container-fluid">
+    <form action="" method="POST">
+        <div class="p-3 mt-2 mx-auto shadow rounded" style="width: 100%; max-width: 310px;">
+            <h2 class="text-center">My University</h2>
+            <img src="<?= ROOT; ?>/assets/logo.png" class="rounded-circle w-50 d-block mx-auto border border-dark">
+            <h3>Add user</h3>
+            <input type="firstname" class="my-2 form-control" name="firstname" placeholder="Firstname" autofocus>
+            <input type="lastname" class="my-2 form-control" name="lastname" placeholder="Lastname">
 
-                <select class="my-2 form-control" name="gender">
-                    <option value="">--Select a Gender--</option>
-                    <option value="male">Male</option>
-                    <option value="female">Female</option>
-                </select>
-                <select class="my-2 form-control" name="rank">
-                    <option value="">--Select a Rank--</option>
-                    <option value="student">Student</option>
-                    <option value="reception">Receprion</option>
-                    <option value="lecturer">Lecturer</option>
-                    <option value="admin">Admin</option>
-                    <option value="super">Super Admin</option>
-                </select>
+            <select class="my-2 form-control" name="gender">
+                <option value="">--Select a Gender--</option>
+                <option value="male">Male</option>
+                <option value="female">Female</option>
+            </select>
+            <select class="my-2 form-control" name="rank">
+                <option value="">--Select a Rank--</option>
+                <option value="student">Student</option>
+                <option value="reception">Receprion</option>
+                <option value="lecturer">Lecturer</option>
+                <option value="admin">Admin</option>
+                <option value="super">Super Admin</option>
+            </select>
 
-                <input type="email" class="my-2 form-control" name="email" placeholder="Email">
-                <input type="password" class="my-2 form-control" name="password" placeholder="Password">
-                <input type="password" class="my-2 form-control" name="password2" placeholder="Confirm password">
-                <br>
-                <button class="btn btn-danger text-white">Cancel</button>
-                <button type="button" class="btn btn-primary float-end">Add user</button>
-            </div>
-        </form>
-    </div>
+            <input type="email" class="my-2 form-control" name="email" placeholder="Email">
+            <input type="password" class="my-2 form-control" name="password" placeholder="Password">
+            <input type="password" class="my-2 form-control" name="password2" placeholder="Confirm password">
+            <br>
+            <button class="btn btn-danger text-white">Cancel</button>
+            <button type="submit" class="btn btn-primary float-end">Add user</button>
+        </div>
+    </form>
+</div>
