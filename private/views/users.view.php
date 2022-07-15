1f001d6fda30357d029b5e25d0946ee3f776ba06
IvansMuzicenko <ivans.muzicenko@gmail.com>
2022-07-15 08:24:48 +0300

    feat: data check

--- private/views/users.view.php
+++ private/views/users.view.php
@@ -2,17 +2,21 @@
     <?php require "../private/views/includes/crumbs.view.php"; ?>
 
     <div class="card-group justify-content-center">
-        <?php foreach ($rows as $row) : ?>
+        <?php if ($rows) : ?>
+            <?php foreach ($rows as $row) : ?>
 
-            <div class="card m-2 shadow-sm" style="max-width: 14rem;min-width: 14rem;">
-                <img src="<?= ASSETS; ?>/user_female.jpg" class="card-img-top" alt="Card image">
-                <div class="card-body">
-                    <h5 class="card-title"><?= $row->firstname ?> <?= $row->lastname ?></h5>
-                    <p class="card-text"><?= str_replace("_", " ", $row->rank) ?></p>
-                    <a href="#" class="btn btn-primary">Details</a>
+                <div class="card m-2 shadow-sm" style="max-width: 14rem;min-width: 14rem;">
+                    <img src="<?= ASSETS; ?>/user_female.jpg" class="card-img-top" alt="Card image">
+                    <div class="card-body">
+                        <h5 class="card-title"><?= $row->firstname ?> <?= $row->lastname ?></h5>
+                        <p class="card-text"><?= str_replace("_", " ", $row->rank) ?></p>
+                        <a href="#" class="btn btn-primary">Details</a>
+                    </div>
                 </div>
-            </div>
-        <?php endforeach; ?>
+            <?php endforeach; ?>
+        <?php else : ?>
+            <h4>No users were found</h4>
+        <?php endif; ?>
 
     </div>
 
